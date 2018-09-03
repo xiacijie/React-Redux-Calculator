23d0ca1b753a3d2258be1b34621244bd117692d8
xiacijie <cijie@ualberta.ca>
2018-09-02 23:03:52 -0600

    delete stack

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -14,48 +14,105 @@ class Calculator extends Component {
                 {value: "C", handle: this.clearCurrent, functional: true},
                 {value: "%", handle: this.toPercentage, functional: true},
                 {value: "➗", handle: this.clearAll, functional: true},
-                {value: 7, handle: this.addDigit},
-                {value: 8, handle: this.addDigit},
-                {value: 9, handle: this.addDigit},
+                {value: "7", handle: this.addDigit},
+                {value: "8", handle: this.addDigit},
+                {value: "9", handle: this.addDigit},
                 {value: "✖", handle: this.multiply, functional: true},
-                {value: 4, handle: this.addDigit},
-                {value: 5, handle: this.addDigit},
-                {value: 6, handle: this.addDigit},
+                {value: "4", handle: this.addDigit},
+                {value: "5", handle: this.addDigit},
+                {value: "6", handle: this.addDigit},
                 {value: "-", handle: this.minus, functional: true},
-                {value: 1, handle: this.addDigit},
-                {value: 2, handle: this.addDigit},
-                {value: 3, handle: this.addDigit},
+                {value: "1", handle: this.addDigit},
+                {value: "2", handle: this.addDigit},
+                {value: "3", handle: this.addDigit},
                 {value: "+", handle: this.plus, functional: true},
                 {value: "+/-", handle: this.negate, functional: true},
-                {value: 0, handle: this.addDigit},
+                {value: "0", handle: this.addDigit},
                 {value: ".", handle: this.addDigit, functional:true},
                 {value: "=", handle: this.equal, functional: true}
             ],
-            stack: new Stack(),
-            operatorStack: new Stack(),
+            stack: [],
+            operatorStack: [],
             opertatorState: false
         }
      
     }
 
-    componentDidMount = () =>{
+    // Display result, any input will overwrite the current display
+    resultMode = true;
+    operatorMode = true;
+
+    getStack = () =>{
         const {stack} = this.state;
-        const newStack = Object.assign({},stack);
-        newStack.push(0);
+        const length = stack.length;
+        return {stack,length};
+    }
+
+    componentDidMount = () =>{
+        const {stack} = this.getStack();
+        const newStack = stack.slice(0);
+        newStack.push("0");
         this.setState({stack:newStack});
     }
 
     addDigit = (digit) =>{
-        
+        const {stack,length} = this.getStack();
+        let lastNum = stack[length - 1]; // last number in the stack
+        if (this.resultMode){
+            lastNum = "";
+            this.resultMode = false;
+        }
+
+        // add dot
+        if (digit == "."){
+            // already has the dot
+            if (lastNum.indexOf(".") > -1){
+                return;
+            }
+
+            if (lastNum == ""){
+                lastNum += "0";
+            }
+        }
+
+
+        lastNum += String(digit);
+
+        const newStack = stack.slice(0);
+        newStack[length - 1] = lastNum;
+        this.setState({stack: newStack});
+
     }
 
-    negate = () =>{
+    
 
+    negate = () =>{
+        const {stack,length} = this.getStack();
+        let lastNum = stack[length - 1];
+        if (lastNum == 0){
+            return;
+        }
+        
+        lastNum = String(-lastNum);
+        
+
+        const newStack = stack.slice(0);
+        newStack[length - 1] = lastNum;
+        this.setState({stack:newStack});
        
     }
 
     toPercentage = () =>{
-       
+       const {stack, length} = this.getStack();
+       let lastNum = stack[length-1];
+       if (lastNum == "0"){
+           return;
+       }
+
+       lastNum = String(lastNum / 100);
+       const newStack = stack.slice(0);
+       newStack[length-1] = lastNum;
+       this.setState({stack: newStack});
     }
 
     divide = () =>{
@@ -83,14 +140,15 @@ class Calculator extends Component {
     }
 
     clearAll = () =>{
-        this.setState({stack:[]});
+        this.setState({stack:["0"]});
+        this.resultMode = true;
     }
     
 
     render() {
-        const stack = this.state.stack;
+        const {stack} = this.state;
         const length = stack.length;
-        const show = stack.peek();
+        const show = stack[length-1];
         return (
         <div className="calculator">
             <Display result={show}/>
